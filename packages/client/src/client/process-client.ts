c9b11595c31f494f02d83e1a842e88969b359c93
NOSI / PM - Carlos Alberto Andrade  Graca <carlosa.graca@nosi.cv>
2025-09-07 22:18:27 -0100

    feat(process-client): update createProcessSequence endpoint to include process application base

--- packages/client/src/client/process-client.ts
+++ packages/client/src/client/process-client.ts
@@ -138,10 +138,11 @@ export class ProcessClient extends BaseApiClient {
    */
   async createProcessSequence(
     processDefinitionId: string,
+    processApplicationBase: string,
     sequence: CreateProcessSequenceRequest,
   ): Promise<ApiResponse<ProcessSequence>> {
     return this.post<ProcessSequence>(
-      `/process-definitions/${processDefinitionId}/sequence`,
+      `/process-definitions/${processDefinitionId}/applications/${processApplicationBase}/sequence`,
       sequence,
     );
   }
